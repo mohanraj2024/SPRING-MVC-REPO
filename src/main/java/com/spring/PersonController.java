025d7e8cd03632817b2799fb2bb0259527ca5e91
mohanraj2024 <mohanraj.thangavelu23@gmail.com>
2024-08-12 21:52:53 +0530

    commands updated

--- src/main/java/com/spring/PersonController.java
+++ src/main/java/com/spring/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.service.PersonService;
-//new class added
+//new controller class added
 @Controller
 public class PersonController {
 	
